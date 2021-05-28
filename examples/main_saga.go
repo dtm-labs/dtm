faca20d75638e69c510e9a3427728ad45bfb539d
yedongfu <dongfuye@163.com>
2021-05-28 20:29:42 +0800

    saga should refactor next

---
+++ examples/main_saga.go
@@ -0,0 +1,105 @@
+package examples
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/yedf/dtm"
+	"github.com/yedf/dtm/common"
+)
+
+// 事务参与者的服务地址
+const SagaBusiPort = 8081
+const SagaBusiApi = "/api/busi_saga"
+
+var SagaBusi = fmt.Sprintf("http://localhost:%d%s", SagaBusiPort, SagaBusiApi)
+
+func SagaMain() {
+	go SagaStartSvr()
+	sagaFireRequest()
+	time.Sleep(1000 * time.Second)
+}
+
+func SagaStartSvr() {
+	logrus.Printf("saga examples starting")
+	app := common.GetGinApp()
+	SagaAddRoute(app)
+	app.Run(fmt.Sprintf(":%d", SagaBusiPort))
+}
+
+func sagaFireRequest() {
+	gid := common.GenGid()
+	logrus.Printf("busi transaction begin: %s", gid)
+	req := &TransReq{
+		Amount:         30,
+		TransInResult:  "SUCCESS",
+		TransOutResult: "SUCCESS",
+	}
+	saga := dtm.SagaNew(DtmServer, gid)
+
+	saga.Add(SagaBusi+"/TransOut", SagaBusi+"/TransOutCompensate", req)
+	saga.Add(SagaBusi+"/TransIn", SagaBusi+"/TransInCompensate", req)
+	err := saga.Prepare(SagaBusi + "/TransQuery")
+	e2p(err)
+	logrus.Printf("busi trans commit")
+	err = saga.Commit()
+	e2p(err)
+}
+
+// api
+
+func SagaAddRoute(app *gin.Engine) {
+	app.POST(SagaBusiApi+"/TransIn", common.WrapHandler(sagaTransIn))
+	app.POST(SagaBusiApi+"/TransInCompensate", common.WrapHandler(sagaTransInCompensate))
+	app.POST(SagaBusiApi+"/TransOut", common.WrapHandler(SagaTransOut))
+	app.POST(SagaBusiApi+"/TransOutCompensate", common.WrapHandler(sagaTransOutCompensate))
+	app.GET(SagaBusiApi+"/TransQuery", common.WrapHandler(sagaTransQuery))
+	logrus.Printf("examples listening at %d", SagaBusiPort)
+}
+
+var SagaTransInResult = ""
+var SagaTransOutResult = ""
+var SagaTransInCompensateResult = ""
+var SagaTransOutCompensateResult = ""
+var SagaTransQueryResult = ""
+
+func sagaTransIn(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(SagaTransInResult, req.TransInResult, "SUCCESS")
+	logrus.Printf("%s TransIn: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func sagaTransInCompensate(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(SagaTransInCompensateResult, "SUCCESS")
+	logrus.Printf("%s TransInCompensate: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func SagaTransOut(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(SagaTransOutResult, req.TransOutResult, "SUCCESS")
+	logrus.Printf("%s TransOut: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func sagaTransOutCompensate(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(SagaTransOutCompensateResult, "SUCCESS")
+	logrus.Printf("%s TransOutCompensate: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func sagaTransQuery(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	logrus.Printf("%s TransQuery", gid)
+	res := common.OrString(SagaTransQueryResult, "SUCCESS")
+	return M{"result": res}, nil
+}
---
+++ examples/main_tcc.go
@@ -0,0 +1,125 @@
+package examples
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/yedf/dtm"
+	"github.com/yedf/dtm/common"
+)
+
+// 事务参与者的服务地址
+const TccBusiPort = 8083
+const TccBusiApi = "/api/busi_tcc"
+
+var TccBusi = fmt.Sprintf("http://localhost:%d%s", TccBusiPort, TccBusiApi)
+
+func TccMain() {
+	go TccStartSvr()
+	tccFireRequest()
+	time.Sleep(1000 * time.Second)
+}
+
+func TccStartSvr() {
+	logrus.Printf("tcc examples starting")
+	app := common.GetGinApp()
+	TccAddRoute(app)
+	app.Run(fmt.Sprintf(":%d", TccBusiPort))
+}
+
+func tccFireRequest() {
+	gid := common.GenGid()
+	logrus.Printf("busi transaction begin: %s", gid)
+	req := &TransReq{
+		Amount:         30,
+		TransInResult:  "SUCCESS",
+		TransOutResult: "SUCCESS",
+	}
+	tcc := dtm.TccNew(DtmServer, gid)
+
+	tcc.Add(TccBusi+"/TransOutTry", TccBusi+"/TransOutConfirm", TccBusi+"/TransOutCancel", req)
+	tcc.Add(TccBusi+"/TransInTry", TccBusi+"/TransInConfirm", TccBusi+"/TransOutCancel", req)
+	err := tcc.Prepare(TccBusi + "/TransQuery")
+	e2p(err)
+	logrus.Printf("busi trans commit")
+	err = tcc.Commit()
+	e2p(err)
+}
+
+// api
+
+func TccAddRoute(app *gin.Engine) {
+	app.POST(TccBusiApi+"/TransInTry", common.WrapHandler(tccTransInTry))
+	app.POST(TccBusiApi+"/TransInConfirm", common.WrapHandler(tccTransInConfirm))
+	app.POST(TccBusiApi+"/TransInCancel", common.WrapHandler(tccTransCancel))
+	app.POST(TccBusiApi+"/TransOutTry", common.WrapHandler(tccTransOutTry))
+	app.POST(TccBusiApi+"/TransOutConfirm", common.WrapHandler(tccTransOutConfirm))
+	app.POST(TccBusiApi+"/TransOutCancel", common.WrapHandler(tccTransOutCancel))
+	app.GET(TccBusiApi+"/TransQuery", common.WrapHandler(tccTransQuery))
+	logrus.Printf("examples listening at %d", TccBusiPort)
+}
+
+var TccTransInTryResult = ""
+var TccTransOutTryResult = ""
+var TccTransInCancelResult = ""
+var TccTransOutCancelResult = ""
+var TccTransInConfirmResult = ""
+var TccTransOutConfirmResult = ""
+var TccTransQueryResult = ""
+
+func tccTransInTry(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(TccTransInTryResult, req.TransInResult, "SUCCESS")
+	logrus.Printf("%s TransInTry: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func tccTransInConfirm(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(TccTransInConfirmResult, "SUCCESS")
+	logrus.Printf("%s tccTransInConfirm: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func tccTransCancel(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(TccTransInCancelResult, "SUCCESS")
+	logrus.Printf("%s tccTransCancel: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func tccTransOutTry(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(TccTransOutTryResult, req.TransOutResult, "SUCCESS")
+	logrus.Printf("%s TransOut: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func tccTransOutConfirm(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(TccTransOutConfirmResult, "SUCCESS")
+	logrus.Printf("%s TransOutConfirm: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func tccTransOutCancel(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	req := transReqFromContext(c)
+	res := common.OrString(TccTransOutCancelResult, "SUCCESS")
+	logrus.Printf("%s tccTransOutCancel: %v result: %s", gid, req, res)
+	return M{"result": res}, nil
+}
+
+func tccTransQuery(c *gin.Context) (interface{}, error) {
+	gid := c.Query("gid")
+	logrus.Printf("%s TransQuery", gid)
+	res := common.OrString(TccTransQueryResult, "SUCCESS")
+	return M{"result": res}, nil
+}
