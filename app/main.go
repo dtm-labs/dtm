cee73457c4792cfcb79016573d11cf53e47f400b
yedongfu <dongfuye@163.com>
2021-07-10 23:07:33 +0800

    update examples

--- app/main.go
+++ app/main.go
@@ -18,14 +18,16 @@ func wait() {
 }
 
 func main() {
-	if len(os.Args) == 1 || os.Args[1] == "dtmsvr" { // 只启动dtmsvr
+	if len(os.Args) > 1 && os.Args[1] == "dtmsvr" { // 实际运行，只启动dtmsvr，不重新load数据
 		dtmsvr.MainStart()
 		wait()
 	}
 	// 下面都是运行示例，因此首先把服务器的数据重新准备好
 	dtmsvr.PopulateMysql()
 	dtmsvr.MainStart()
-
+	if len(os.Args) == 1 { // 默认没有参数的情况下，准备好数据并启动dtmsvr即可
+		wait()
+	}
 	// quick_start 比较独立，单独作为一个例子运行，方便新人上手
 	if len(os.Args) > 1 && (os.Args[1] == "quick_start" || os.Args[1] == "qs") {
 		examples.QuickStarMain()
@@ -41,13 +43,21 @@ func main() {
 	} else if os.Args[1] == "saga" { // 启动saga示例
 		examples.SagaSetup(app)
 		examples.SagaFireRequest()
+	} else if os.Args[1] == "tcc" { // 启动tcc示例
+		examples.TccSetup(app)
+		examples.TccFireRequest()
+	} else if os.Args[1] == "msg" { // 启动msg示例
+		examples.MsgSetup(app)
+		examples.MsgFireRequest()
 	} else if os.Args[1] == "all" { // 运行所有示例
 		examples.SagaSetup(app)
 		examples.TccSetup(app)
 		examples.XaSetup(app)
+		examples.MsgSetup(app)
 		examples.SagaFireRequest()
 		examples.TccFireRequest()
 		examples.XaFireRequest()
+		examples.MsgFireRequest()
 	} else if os.Args[1] == "saga_barrier" {
 		examples.SagaBarrierAddRoute(app)
 		examples.SagaBarrierFireRequest()
