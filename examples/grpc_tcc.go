8f011d4527cd2a6515a5c5a0dfe867ee2e9f3524
yedf2 <120050102@qq.com>
2021-08-11 20:54:25 +0800

    sample use register

---
+++ examples/grpc_tcc.go
@@ -0,0 +1,24 @@
+package examples
+
+import (
+	"github.com/yedf/dtm/dtmcli"
+	dtmgrpc "github.com/yedf/dtm/dtmgrpc"
+)
+
+func init() {
+	addSample("grpc_tcc", func() string {
+		dtmcli.Logf("tcc simple transaction begin")
+		gid := dtmgrpc.MustGenGid(DtmGrpcServer)
+		err := dtmgrpc.TccGlobalTransaction(DtmGrpcServer, gid, func(tcc *dtmgrpc.TccGrpc) error {
+			data := dtmcli.MustMarshal(&TransReq{Amount: 30})
+			_, err := tcc.CallBranch(data, BusiGrpc+"/examples.Busi/TransOut", BusiGrpc+"/examples.Busi/TransOutConfirm", BusiGrpc+"/examples.Busi/TransOutRevert")
+			if err != nil {
+				return err
+			}
+			_, err = tcc.CallBranch(data, BusiGrpc+"/examples.Busi/TransIn", BusiGrpc+"/examples.Busi/TransInConfirm", BusiGrpc+"/examples.Busi/TransInRevert")
+			return err
+		})
+		dtmcli.FatalIfError(err)
+		return gid
+	})
+}
