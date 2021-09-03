00f68d9b1594f65fb10a165991212e17500de51b
yedf2 <120050102@qq.com>
2021-09-03 22:13:22 +0800

    add docker pub

--- helper/build-repo.sh
+++ helper/build-repo.sh
@@ -1,2 +1,8 @@
 set -x
+if [ x$1 == x ]; then
+  echo please specify version
+  exit 1
+fi
+
 docker build -f helper/Dockerfile-release -t yedf/dtm:latest . && docker push yedf/dtm:latest
+docker tag yedf/dtm:latest yedf/dtm:$1 && docker push yedf/dtm:$1
