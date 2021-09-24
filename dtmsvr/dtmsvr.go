968faa28ab87b859457d134dcd86e58eeceae80a
Wang Zhuo <zhuowangy2k@outlook.com>
2021-09-24 17:52:00 +0800

    support prometheus

--- dtmsvr/dtmsvr.go
+++ dtmsvr/dtmsvr.go
@@ -9,6 +9,7 @@ import (
 	"github.com/yedf/dtm/dtmcli"
 	"github.com/yedf/dtm/dtmgrpc"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
 
 	"github.com/yedf/dtm/examples"
@@ -16,18 +17,23 @@ import (
 
 var dtmsvrPort = 8080
 var dtmsvrGrpcPort = 58080
+var metricsPort = 8889
 
 // StartSvr StartSvr
 func StartSvr() {
 	dtmcli.Logf("start dtmsvr")
 	app := common.GetGinApp()
+	app = HTTP_metrics(app)
 	addRoute(app)
 	dtmcli.Logf("dtmsvr listen at: %d", dtmsvrPort)
 	go app.Run(fmt.Sprintf(":%d", dtmsvrPort))
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", dtmsvrGrpcPort))
 	dtmcli.FatalIfError(err)
-	s := grpc.NewServer(grpc.UnaryInterceptor(dtmgrpc.GrpcServerLog))
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc.UnaryServerInterceptor(GRPC_metrics), grpc.UnaryServerInterceptor(dtmgrpc.GrpcServerLog)),
+		))
 	dtmgrpc.RegisterDtmServer(s, &dtmServer{})
 	dtmcli.Logf("grpc listening at %v", lis.Addr())
 	go func() {
@@ -35,6 +41,9 @@ func StartSvr() {
 		dtmcli.FatalIfError(err)
 	}()
 
+	// prometheus exporter
+	dtmcli.Logf("prometheus exporter listen at: %d", metricsPort)
+	PrometheusHttpRun(fmt.Sprintf("%d", metricsPort))
 	time.Sleep(100 * time.Millisecond)
 }
 
--- go.mod
+++ go.mod
@@ -8,17 +8,17 @@ require (
 	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-resty/resty/v2 v2.6.0
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.11.0
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	google.golang.org/genproto v0.0.0-20210805201207-89edb61ffb67 // indirect
 	google.golang.org/grpc v1.39.1
 	google.golang.org/protobuf v1.27.1
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.21.12
