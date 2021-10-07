b7d17f76c96ca444cf1d7034e1c169facb72c4ce
yedf2 <120050102@qq.com>
2021-10-07 22:16:33 +0800

    bench scripts updated

---
+++ bench/setup.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+# install all commands needed
+apt install -y git sysbench apache2-utils mysql-client-core-8.0
+
+# install docker and docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# install go
+wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
