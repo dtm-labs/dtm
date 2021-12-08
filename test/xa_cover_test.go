1b81ac769c76142e13515293f3fe21f79e6665f3
yedf2 <120050102@qq.com>
2021-12-08 18:41:14 +0800

    update xa cover

--- test/xa_cover_test.go
+++ test/xa_cover_test.go
@@ -20,9 +20,12 @@ func TestXaCoverDBError(t *testing.T) {
 		getXc().Conf["driver"] = "no-driver"
 		_, err = xa.CallBranch(req, examples.Busi+"/TransInXa")
 		assert.Error(t, err)
+		getXc().Conf["driver"] = oldDriver // make abort succeed
 		return nil, err
 	})
 	assert.Error(t, err)
+	getXc().Conf["driver"] = "no-driver" // make xa rollback failed
+	waitTransProcessed(gid)
 	getXc().Conf["driver"] = oldDriver
 	cronTransOnceForwardNow(500) // rollback succeeded here
 	assert.Equal(t, StatusFailed, getTransStatus(gid))
@@ -37,4 +40,15 @@ func TestXaCoverDTMError(t *testing.T) {
 	})
 	assert.Error(t, err)
 	getXc().Server = oldServer
+}
+
+func TestXaCoverGidError(t *testing.T) {
+	gid := "errgid-'  '"
+	err := getXc().XaGlobalTransaction(gid, func(xa *dtmcli.Xa) (*resty.Response, error) {
+		req := examples.GenTransReq(30, false, false)
+		_, err := xa.CallBranch(req, examples.Busi+"/TransOutXa")
+		assert.Error(t, err)
+		return nil, err
+	})
+	assert.Error(t, err)
 }
