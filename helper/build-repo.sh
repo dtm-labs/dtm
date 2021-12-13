d266c118bf1da34dfa0463380cd32aaa1ba050d1
jxlwqq <jxlwqq@gmail.com>
2021-12-13 15:26:34 +0800

    ci: support multi-platform docker image build

--- helper/build-repo.sh
+++
@@ -1,8 +0,0 @@
-set -x
-if [ x$1 == x ]; then
-  echo please specify version
-  exit 1
-fi
-
-docker build -f helper/Dockerfile-release -t yedf/dtm:latest . && docker push yedf/dtm:latest
-docker tag yedf/dtm:latest yedf/dtm:$1 && docker push yedf/dtm:$1
