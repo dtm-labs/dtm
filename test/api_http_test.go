3e685aa39ef516a62c8c56b71e3938ebc93cfb26
lixiaoshuang <644968328@qq.com>
2021-12-20 12:38:24 +0800

    add test

--- test/api_http_test.go
+++ test/api_http_test.go
@@ -1,18 +1,12 @@
-package dtmsvr
+package test
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/yedf/dtm/common"
-	"github.com/yedf/dtm/dtmcli"
 	"github.com/yedf/dtm/dtmcli/dtmimp"
 	"testing"
 )
 
 func TestDtmMetrics(t *testing.T) {
-	common.MustLoadConfig()
-	dtmcli.SetCurrentDBType(common.Config.ExamplesDB.Driver)
-	PopulateDB(true)
-	StartSvr()
 	rest, err := dtmimp.RestyClient.R().Get("http://localhost:36789/api/metrics")
 	assert.Nil(t, err)
 	assert.Equal(t, rest.StatusCode(), 200)
