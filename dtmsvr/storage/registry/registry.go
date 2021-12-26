f80368deb9b635698476bd7f2a78ab51ca971a8b
houseme <qzg40737@163.com>
2021-12-26 21:25:10 +0800

    feat: improve update boltdb storage driver documentation

--- dtmsvr/storage/registry/registry.go
+++ dtmsvr/storage/registry/registry.go
@@ -1,31 +1,32 @@
 package registry
 
 import (
-	"time"
-
-	"github.com/dtm-labs/dtm/common"
-	"github.com/dtm-labs/dtm/dtmsvr/storage"
-	"github.com/dtm-labs/dtm/dtmsvr/storage/boltdb"
-	"github.com/dtm-labs/dtm/dtmsvr/storage/redis"
-	"github.com/dtm-labs/dtm/dtmsvr/storage/sql"
+    "time"
+    
+    "github.com/dtm-labs/dtm/common"
+    "github.com/dtm-labs/dtm/dtmsvr/storage"
+    "github.com/dtm-labs/dtm/dtmsvr/storage/boltdb"
+    "github.com/dtm-labs/dtm/dtmsvr/storage/redis"
+    "github.com/dtm-labs/dtm/dtmsvr/storage/sql"
 )
 
 var config = &common.Config
 
 var stores map[string]storage.Store = map[string]storage.Store{
-	"redis":    &redis.RedisStore{},
-	"mysql":    &sql.SqlStore{},
-	"postgres": &sql.SqlStore{},
-	"boltdb":   &boltdb.BoltdbStore{},
+    "redis":    &redis.RedisStore{},
+    "mysql":    &sql.SqlStore{},
+    "postgres": &sql.SqlStore{},
+    "boltdb":   &boltdb.BoltdbStore{},
 }
 
+// GetStore get storage driver
 func GetStore() storage.Store {
-	return stores[config.Store.Driver]
+    return stores[config.Store.Driver]
 }
 
 // WaitStoreUp wait for db to go up
 func WaitStoreUp() {
-	for err := GetStore().Ping(); err != nil; err = GetStore().Ping() {
-		time.Sleep(3 * time.Second)
-	}
+    for err := GetStore().Ping(); err != nil; err = GetStore().Ping() {
+        time.Sleep(3 * time.Second)
+    }
 }
