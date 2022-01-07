ce76ded47da9428e6e7635c0bbd8bbd46fc25677
liulei <liulei@xiaozhu.com>
2022-01-07 18:16:12 +0800

    fix: bench dir ci error and enable ci lint check

--- bench/svr/http.go
+++ bench/svr/http.go
@@ -65,10 +65,11 @@ func reloadData() {
 	logger.Debugf("%d users inserted. used: %dms", total, time.Since(began).Milliseconds())
 }
 
-var uidCounter int32 = 0
-var mode string = ""
-var sqls int = 1
+var uidCounter int32
+var mode string
+var sqls = 1
 
+// PrepareBenchDB prepares db data for bench
 func PrepareBenchDB() {
 	db := pdbGet()
 	_, err := dtmimp.DBExec(db, "drop table if exists dtm_busi.user_account_log")
@@ -95,7 +96,9 @@ func StartSvr() {
 	app := dtmutil.GetGinApp()
 	benchAddRoute(app)
 	logger.Debugf("bench listening at %d", benchPort)
-	go app.Run(fmt.Sprintf(":%s", benchPort))
+	go func() {
+		_ = app.Run(fmt.Sprintf(":%s", benchPort))
+	}()
 }
 
 func qsAdjustBalance(uid int, amount int, c *gin.Context) (interface{}, error) {
@@ -116,11 +119,13 @@ func qsAdjustBalance(uid int, amount int, c *gin.Context) (interface{}, error) {
 	if strings.Contains(mode, "barrier") {
 		barrier, err := dtmcli.BarrierFromQuery(c.Request.URL.Query())
 		logger.FatalIfError(err)
-		barrier.Call(txGet(), f)
+		err = barrier.Call(txGet(), f)
+		logger.FatalIfError(err)
 	} else {
 		tx := txGet()
-		f(tx)
-		err := tx.Commit()
+		err := f(tx)
+		logger.FatalIfError(err)
+		err = tx.Commit()
 		logger.FatalIfError(err)
 	}
 
