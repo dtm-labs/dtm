3fe9888e0a669003f19c5d3bff3c6bb5118c05ff
KuiLiao <horsekiao@gmail.com>
2022-03-29 12:19:41 +0800

    fix database not exist

--- helper/bench/prepare.sh
+++ helper/bench/prepare.sh
@@ -7,4 +7,4 @@ git clone https://github.com/dtm-labs/dtm.git && cd dtm && git checkout alpha &&
 echo 'all prepared. you shoud run following commands to test in different terminal'
 echo
 echo 'cd dtf && go run helper/bench/main.go redis|boltdb|db'
-echo 'cd dtf/helper && bench/run-redis|boltdb|mysql.sh'
+echo 'cd dtf && ./helper/bench/test-redis|boltdb|mysql.sh'
