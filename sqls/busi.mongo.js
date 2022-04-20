99335937fe63a908a671a366a2a17c5349b0a4a8
yedf2 <120050102@qq.com>
2022-04-20 11:42:23 +0800

    update mongo scripts

--- sqls/busi.mongo.js
+++ sqls/busi.mongo.js
@@ -1,3 +1,6 @@
-use busi
-db.busi.insert({user_id: 1, balance: 10000})
-db.busi.insert({user_id: 2, balance: 10000})
+use dtm_busi
+db.user_account.drop()
+db.user_account.createIndex({ user_id: NumberLong(1) }, { unique: true })
+
+db.user_account.insert({ user_id: NumberLong(1), balance: 10000 })
+db.user_account.insert({ user_id: NumberLong(2), balance: 10000 })
