cdf74ebdae474b31d2ce6f720e4a89bc44488c8c
Pengfeix <2436173309@qq.com>
2022-06-30 21:37:15 +0800

    add mongo dir

---
+++ dtmsvr/storage/mongo/mongo.go
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 yedf. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package sql
+
+import (
+	"github.com/dtm-labs/dtm/dtmsvr/config"
+)
+
+var conf = &config.Config
+
+// Store implements storage.Store, and storage with db
+type Store struct {
+}
