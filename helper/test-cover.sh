3cc2dcb74ffc961fb3d5da7db52235d373af8655
yedf2 <120050102@qq.com>
2022-07-10 21:04:37 +0800

    test-cover shorted

--- helper/test-cover.sh
+++ helper/test-cover.sh
@@ -2,7 +2,7 @@ set -x
 echo "mode: count" > coverage.txt
 for store in redis boltdb mysql postgres; do
   for d in $(go list ./... | grep -v vendor); do
-    TEST_STORE=$store go test -failfast -covermode count -coverprofile=profile.out -coverpkg=github.com/dtm-labs/dtm/client/dtmcli,github.com/dtm-labs/dtm/client/dtmcli/dtmimp,github.com/dtm-labs/dtm/client/dtmcli/logger,github.com/dtm-labs/dtm/client/dtmgrpc,github.com/dtm-labs/dtm/client/workflow,github.com/dtm-labs/dtm/client/dtmgrpc/dtmgimp,github.com/dtm-labs/dtm/dtmsvr,github.com/dtm-labs/dtm/dtmsvr/config,github.com/dtm-labs/dtm/dtmsvr/storage,github.com/dtm-labs/dtm/dtmsvr/storage/boltdb,github.com/dtm-labs/dtm/dtmsvr/storage/redis,github.com/dtm-labs/dtm/dtmsvr/storage/registry,github.com/dtm-labs/dtm/dtmsvr/storage/sql,github.com/dtm-labs/dtm/dtmutil -gcflags=-l $d || exit 1
+    TEST_STORE=$store go test -failfast -covermode count -coverprofile=profile.out -coverpkg=client/dtmcli,client/dtmcli/dtmimp,client/dtmcli/logger,client/dtmgrpc,client/workflow,client/dtmgrpc/dtmgimp,dtmsvr,dtmsvr/config,dtmsvr/storage,dtmsvr/storage/boltdb,dtmsvr/storage/redis,dtmsvr/storage/registry,dtmsvr/storage/sql,dtmutil -gcflags=-l $d || exit 1
       if [ -f profile.out ]; then
           cat profile.out | grep -v 'mode:' >> coverage.txt
           echo > profile.out
