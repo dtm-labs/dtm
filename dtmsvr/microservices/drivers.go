58da68a57dabc5dd92ba1c77ffa3d45f672ccb70
vicnoah <vicroah@gmail.com>
2022-10-07 18:37:12 +0800

    🌈 style(drivers.go): go fmt

--- dtmsvr/microservices/drivers.go
+++ dtmsvr/microservices/drivers.go
@@ -3,9 +3,9 @@ package microservices
 import (
 	// load the microserver drivers
 	_ "github.com/dtm-labs/dtmdriver-dapr"
+	_ "github.com/dtm-labs/dtmdriver-ego"
 	_ "github.com/dtm-labs/dtmdriver-gozero"
 	_ "github.com/dtm-labs/dtmdriver-kratos"
 	_ "github.com/dtm-labs/dtmdriver-polaris"
 	_ "github.com/dtm-labs/dtmdriver-springcloud"
-	_ "github.com/dtm-labs/dtmdriver-ego"
 )
