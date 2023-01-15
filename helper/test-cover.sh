a3df4cd01e51f3369078f5c1ab162f8168d5a6ef
yedf2 <120050102@qq.com>
2023-01-15 21:24:49 +0800

    add DTM_DEBUG=1 for test

--- helper/test-cover.sh
+++ helper/test-cover.sh
@@ -1,4 +1,5 @@
 set -x
+export DTM_DEBUG=1
 echo "mode: count" > coverage.txt
 for store in redis boltdb mysql postgres; do
   TEST_STORE=$store go test -failfast -covermode count -coverprofile=profile.out -coverpkg=github.com/dtm-labs/dtm/client/dtmcli,github.com/dtm-labs/dtm/client/dtmcli/dtmimp,github.com/dtm-labs/logger,github.com/dtm-labs/dtm/client/dtmgrpc,github.com/dtm-labs/dtm/client/workflow,github.com/dtm-labs/dtm/client/dtmgrpc/dtmgimp,github.com/dtm-labs/dtm/dtmsvr,dtmsvr/config,github.com/dtm-labs/dtm/dtmsvr/storage,github.com/dtm-labs/dtm/dtmsvr/storage/boltdb,github.com/dtm-labs/dtm/dtmsvr/storage/redis,github.com/dtm-labs/dtm/dtmsvr/storage/registry,github.com/dtm-labs/dtm/dtmsvr/storage/sql,github.com/dtm-labs/dtm/dtmutil -gcflags=-l ./... || exit 1
